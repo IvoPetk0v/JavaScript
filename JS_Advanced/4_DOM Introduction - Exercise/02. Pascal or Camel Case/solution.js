f71df4bac346c6a6a3963f86b6a9164d251dd7be
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-09-25 20:24:38 +0300

    upload solution

--- JS_Advanced/4_DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
+++ JS_Advanced/4_DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO...
+  let input = document.getElementById("text").value.toLowerCase();
+  let currCase = document.getElementById("naming-convention").value;
+  let result = "";
+  if (currCase === "Camel Case") {
+    let array = input.split(" ");
+    result += array[0];
+    for (let i = 1; i < array.length; i++) {
+      const element = array[i];
+      result += element.charAt(0).toUpperCase() + element.slice(1);
+    }
+  } else if (currCase === "Pascal Case") {
+    let array = input.split(" ");
+    for (let i = 0; i < array.length; i++) {
+      const element = array[i];
+      result += element.charAt(0).toUpperCase() + element.slice(1);
+    }
+  } else {
+    result = "Error!";
+  }
+  document.getElementById("result").innerText = result;
 }
