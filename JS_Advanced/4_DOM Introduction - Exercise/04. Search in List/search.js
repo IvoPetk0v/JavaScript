e2d750d88492412f4f9d224e44e97dc1aa858dbd
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-09-26 20:53:38 +0300

    upload solution

--- JS_Advanced/4_DOM Introduction - Exercise/04. Search in List/search.js
+++ JS_Advanced/4_DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,17 @@
 function search() {
-   // TODO
+   let towns = Array.from(document.querySelectorAll('#towns li'));
+   let text = document.getElementById("searchText").value;
+   let matchedElement = document.getElementById("result");
+   let count = 0;
+   for (const town of towns) {
+      if (town.textContent.includes(text) && text != "") {
+         count++;
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = 'underline';
+      } else {
+         town.style.fontWeight = 'normal';
+         town.style.textDecoration = 'none';
+      }
+   }
+   matchedElement.textContent = `${count} matches found`
 }
