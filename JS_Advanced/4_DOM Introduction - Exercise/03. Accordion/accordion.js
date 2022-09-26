e5fb3d7ab0aebd93e31384ba092ba2cb7df8688e
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-09-26 20:23:15 +0300

    upload solution

--- JS_Advanced/4_DOM Introduction - Exercise/03. Accordion/accordion.js
+++ JS_Advanced/4_DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    console.log('TODO:...');
+    let buttonName = document.getElementsByClassName("button")[0];
+    let displayStyle = document.getElementById("extra");
+
+    if (buttonName.innerHTML === "More") {
+        displayStyle.style.display = "block";
+        buttonName.textContent = "Less";
+
+    } else {
+        displayStyle.style.display = "none"
+        buttonName.textContent = "More";
+    }
 }
