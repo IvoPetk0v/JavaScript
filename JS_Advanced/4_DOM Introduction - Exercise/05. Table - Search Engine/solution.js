b7444f10e2c4308b0e0249805df78ab755fd26e0
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-10-03 23:13:58 +0300

    upload solution

--- JS_Advanced/4_DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ JS_Advanced/4_DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,16 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let tableRows = document.querySelectorAll("tbody tr");
+      let input = document.getElementById('searchField').value;
 
+      for (const iterator of tableRows) {
+         iterator.classList.remove('select');
+         let text = iterator.innerHTML;
+         if (text.includes(input) && input !== "") {
+            iterator.className = "select";
+         }
+      }
+      input = '';
    }
 }
