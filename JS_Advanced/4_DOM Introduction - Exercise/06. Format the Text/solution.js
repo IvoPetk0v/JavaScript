3e98085658c099e66765eb0b677661241b600704
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-10-05 19:12:25 +0300

    upload solution

--- JS_Advanced/4_DOM Introduction - Exercise/06. Format the Text/solution.js
+++ JS_Advanced/4_DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,14 @@
 function solve() {
-  //TODO
+  let input = document.getElementById('input');
+
+  let sentences = input.value.split('.').filter(ch => ch !== '');
+
+  let output = document.getElementById('output');
+
+  while (sentences.length > 0) {
+    let text = sentences.splice(0, 3).join(". ") + '.';
+    let p = document.createElement('p');
+    p.textContent = text;
+   output.appendChild(p);
+  }
 }
