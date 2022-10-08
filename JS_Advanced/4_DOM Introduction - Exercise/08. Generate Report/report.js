d0caaeb9ba92cacbdab077d506e3797a279f67ac
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-10-08 16:59:14 +0300

    upload solution

--- JS_Advanced/4_DOM Introduction - Exercise/08. Generate Report/report.js
+++ JS_Advanced/4_DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,29 @@
 function generateReport() {
-    //TODO
+    let checkBoxes = document.querySelectorAll('input')
+
+    let markedIndexes = [];
+
+    for (let index = 0; index < checkBoxes.length; index++) {
+        const element = checkBoxes[index];
+        if (element.checked) {
+            markedIndexes.push(index);
+        }
+    }
+    let rows = document.getElementsByTagName('tr');
+    rowsArray = Array.from(rows);
+    rowsArray.shift();
+
+    let objList = [];
+
+    for (let row of rowsArray) {
+        let obj = {};
+        markedIndexes.forEach(index => {
+            let name = checkBoxes[index].name;
+            let rowData = row.children[index].textContent;
+            obj[name] = rowData;
+        });
+        objList.push(obj)
+    }
+    report = JSON.stringify(objList);
+    document.getElementById('output').textContent = report;
 }
