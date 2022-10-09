2a06c381f5d97729da6cabf41b3ad75285e6ba1f
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-10-09 11:32:36 +0300

    upload solution

--- JS_Advanced/5_DOM Events - Exercise/02. Time-Converter/solution.js
+++ JS_Advanced/5_DOM Events - Exercise/02. Time-Converter/solution.js
@@ -1,4 +1,36 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    let buttons = Array.from(document.querySelectorAll("input[type=button]"));
+    buttons.forEach(element => {
+        element.addEventListener("click", convert)
+    });
+
+
+    function convert(event) {
+        let value = Number(event.target.parentElement.querySelector("input[type=text]").value);
+        let unit = event.target.id;
+        switch (unit) {
+            case "daysBtn":
+                populate(value);
+                break;
+            case "hoursBtn":
+                populate(value / 24);
+                break;
+            case "minutesBtn":
+                populate(value / 24 / 60);
+                break;
+            case "secondsBtn":
+                populate(value / 24 / 60 / 60)
+                break;
+        }
+    }
+    function populate(value) {
+        let inputs = Array.from(document.querySelectorAll("input[type=text]"));
+        inputs.shift().value = value;
+        let currValue = value * 24;
+        inputs.forEach(element => {
+            element.value = currValue;
+            currValue *= 60;
+        });
+    }
 }
