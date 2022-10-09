407265b7068acf77dee3578182757763a549d705
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-10-09 13:22:17 +0300

    upload solution

--- JS_Advanced/5_DOM Events - Exercise/03. Locked-Profile/app.js
+++ JS_Advanced/5_DOM Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,23 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = Array.from(document.querySelectorAll("button"));
+    
+    buttons.forEach(element => {
+        element.addEventListener("click", ShowAndHide);
+        document.getElementById
+    });
+
+    function ShowAndHide(e) {
+
+        let checkboxLocked = e.target.parentElement.querySelectorAll("input[type=radio]")[0];
+        let button = e.target.parentElement.querySelector("button");
+        let hiddenDiv = e.target.parentElement.querySelector("div");
+
+        if (!checkboxLocked.checked && button.innerText === 'Show more') {
+            hiddenDiv.style.display = "block";
+            button.innerText = 'Hide it';
+        } else if (!checkboxLocked.checked && button.innerText === 'Hide it') {
+            hiddenDiv.style.display = "none";
+            button.innerText = 'Show more';
+        }
+    }
 }
