78bd52e05aa1c59b0a1b3340c37438774e01ae57
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-10-09 14:12:32 +0300

    upload solution

--- JS_Advanced/5_DOM Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ JS_Advanced/5_DOM Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    let itemText = document.getElementById("newItemText").value;
+    let itemValue = document.getElementById("newItemValue").value;
+    let option = document.createElement("option");
+    let select = document.getElementById("menu");
+
+    option.textContent = itemText;
+    option.value = itemValue;
+    select.appendChild(option);
+    
+    document.getElementById("newItemText").value = "";
+    document.getElementById("newItemValue").value = "";
+
 }
