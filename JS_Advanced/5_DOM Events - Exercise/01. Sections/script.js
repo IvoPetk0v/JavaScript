7cc4e460396eac04cd8ea65720bb0199bfab4d1c
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-10-09 10:35:46 +0300

    upload solution

--- JS_Advanced/5_DOM Events - Exercise/01. Sections/script.js
+++ JS_Advanced/5_DOM Events - Exercise/01. Sections/script.js
@@ -1,3 +1,17 @@
 function create(words) {
-   console.log('TODO:...');
-}
+   let output = document.getElementById("content");
+
+   words.forEach(element => {
+      let div = document.createElement("div");
+      let p = document.createElement("p");
+      p.style.display = "none";
+      p.innerText = element;
+      div.appendChild(p);
+      div.addEventListener("click", ShowP)
+      output.appendChild(div);
+
+   });
+   function ShowP(event) {
+      event.target.children[0].style.display = "";
+   }
+}  
