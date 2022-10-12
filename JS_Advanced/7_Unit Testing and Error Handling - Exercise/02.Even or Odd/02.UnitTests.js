07e24f5a3ed3a501a3a43ead737f69ff1a598437
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-10-12 22:46:52 +0300

    upload solution

--- JS_Advanced/7_Unit Testing and Error Handling - Exercise/02.Even or Odd/02.UnitTests.js
+++ JS_Advanced/7_Unit Testing and Error Handling - Exercise/02.Even or Odd/02.UnitTests.js
@@ -2,7 +2,7 @@ let { assert } = require("chai");
 const { isOddOrEven } = require("./02.Even or Odd");
 
 
-describe("test is Odd or Even checking func work properly", () => {
+describe("test is checking isOddorEven func work properly", () => {
     it("Result should be undefine when input is not type String", () => {
         assert.equal(isOddOrEven(Number(1), undefined));
     });
