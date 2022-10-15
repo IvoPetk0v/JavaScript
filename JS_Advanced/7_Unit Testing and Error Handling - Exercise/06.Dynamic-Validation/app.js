31e353b17269209cacb2c41e506fe4153a793db3
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-10-15 20:15:02 +0300

    upload solution

--- JS_Advanced/7_Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
+++ JS_Advanced/7_Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    // TODO
+    let input = document.getElementById('email');
+    input.addEventListener("change", onChange);
+
+    function onChange(e) {
+        let textInput = e.target.value;
+        let emailPattern = /[a-z0-9]+@[a-z0-9]+\.[a-z0-9]+/g
+
+        if (emailPattern.test(textInput)) {
+            e.target.classList.remove("error")
+        } else {
+            e.target.classList.add("error")
+        }
+    }
 }
