67a41b75fc3c4b01afc1c2790ee6b25a609d62da
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-11-05 12:16:09 +0200

    upload solution

--- JS_Applications/Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
+++ JS_Applications/Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,32 @@
 function solve() {
+    const url = "http://localhost:3030/jsonstore/bus/schedule/";
+    const infoBar = document.getElementById("info").firstChild;
+    const depBtn = document.getElementById("depart");
+    const arrBtt = document.getElementById("arrive");
 
-    function depart() {
-        console.log('Depart TODO...');
+    let nextStopID = "depot";
+    let stopID = "";
+
+    async function depart() {
+        try {
+            const response = await fetch((url + nextStopID));
+            const data = await response.json();
+            nextStopID = data.next;
+            stopID = data.name;
+            infoBar.textContent = `Next stop ${stopID}`;
+            depBtn.disabled = true;
+            arrBtt.disabled = false;
+        } catch (error) {
+            infoBar.textContent = "Error";
+            depBtn.disabled = true;
+            arrBtt.disabled = true;
+        }
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoBar.textContent = `Arriving at ${stopID}`;
+        depBtn.disabled = false;
+        arrBtt.disabled = true;
     }
 
     return {
